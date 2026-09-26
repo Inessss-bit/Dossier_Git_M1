51d4afa10c3d987f38d998ac98f6e76ec12b3fa2
SOGOYOU <ines.sogoyou@etu.univ-tours.fr>
2026-09-26 16:44:15 +0200

    Amelioration visuelle de l'affichage

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 from random import choice
-from pyfiglet import figlet_format
+
 from rich import print
-
-
-print(figlet_format("Citation"))
-print("[cyan]La meilleure façon de prédire l'avenir est de l'inventer.[/cyan]")
+from rich.panel import Panel
+from rich_pyfiglet import RichFiglet
 
 with open("citations.txt", "r", encoding="utf-8") as f:
     citations = f.readlines()
@@ -12,5 +10,17 @@ with open("citations.txt", "r", encoding="utf-8") as f:
 citations = [c.strip() for c in citations]
 citation = choice(citations)
 
-print(figlet_format("Citation"))
-print(f"[cyan]{citation}[/cyan]")
+rich_fig = RichFiglet(
+    "Citation du jour",
+    font="ansi_shadow",
+    colors=["#ff0000", "bright_blue"],
+)
+
+print(rich_fig)
+print(
+    Panel(
+        f"[italic gold1]« {citation} »[/italic gold1]",
+        border_style="gold1",
+        expand=False,
+    )
+)
